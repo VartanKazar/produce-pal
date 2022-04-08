330d2574b36ed298a2fe5be55bd16451c6c4d537
VartanKazar <vartkaze@gmail.com>
2022-04-07 23:42:34 -0700

    Added a shopping card and calendar icon to the navbar.  Changed the home page steps.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars} from "@fortawesome/free-solid-svg-icons"
+import { faBars, faCartShopping, faCalendarAlt } from "@fortawesome/free-solid-svg-icons"
 
 const NavBar = () => {
 
+    //Dummy variables to temporarily make the indexed icons work.
+    //Replace them with the redux <cartItemCount> and <plannerItemCount> variables when available.
+    const cartitemCount = 5;
+    const plannerItemCount = 2;
+
     return (
         <div className='nav-bar'>
-            <FontAwesomeIcon icon={faBars} className="nav-bar-dropdown"/>
+            <FontAwesomeIcon icon={faBars} className="nav-icon-default"/>
 
             <h2>Produce Pal</h2>
 
-            <button>Login</button>
+            <div className='nav-bar-user-section'>
+
+                <div className={`nav-icon-indexed ${plannerItemCount === 0}`} count={plannerItemCount} >
+                    <FontAwesomeIcon icon={faCalendarAlt} className="nav-icon-default"/>
+                </div>
+
+                <div className={`nav-icon-indexed ${cartitemCount === 0}`} count={cartitemCount} >
+                    <FontAwesomeIcon icon={faCartShopping} className="nav-icon-default"/>
+                </div>
+                <button>Login</button>
+            </div>
+
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <IndexedCard 
                 index={1}
                 title="Choose a Recipe"
-                description="Create a recipe from scratch, or select one from your list."
+                description="Create a recipe from scratch, select one from your list, or search for fun new recipes!"
                 />
 
 
@@ -22,18 +22,24 @@ const Home = () => {
 
                 <IndexedCard 
                 index={3}
+                title="Add to Planner"
+                description="Add the recipe to your planner.  You can plan for as many meals as you want!"
+                />
+
+                <IndexedCard 
+                index={4}
                 title="Locate Stores"
                 description="Enter your location and commute distance.  Then search for nearby stores."
                 />
 
                 <IndexedCard 
-                index={4}
+                index={5}
                 title="Select Your Stores"
                 description="Select all the stores you want to shop at, then hit search!"
                 />
 
                 <IndexedCard 
-                index={5}
+                index={6}
                 title="Plan Your Trip"
                 description="Choose where you want to buy each item and save it in your spree list!"
                 />
